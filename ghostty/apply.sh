de7b6aab06e2aa637a1b2fb6bf0b833c7f542162
coffemakingtoaster <mh340@hdm-stuttgart.de>
2025-01-02 09:41:25 +0100

    feat(ghostty): add config cleanup for ghostty

--- ghostty/apply.sh
+++ ghostty/apply.sh
@@ -7,4 +7,10 @@ fi
 
 cp -r ghostty $HOME/.config
 
+# This should (tm) ensure that the application support folder has been created
+# If that is not the case execute the rm command below again after starting ghostty for the first time 
+ghostty +show-config
+
+rm -rf ~/Library/Application Support/com.mitchellh.ghostty/
+
 exit 0
