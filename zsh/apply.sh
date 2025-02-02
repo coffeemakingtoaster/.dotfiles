e5fc45dda8e7963f289610d4bf4649c1462d61e9
coffeemakingtoaster <coffeemakingtoaster@gmail.com>
2025-02-02 14:36:42 +0100

    feat(zsh): set zsh default

--- zsh/apply.sh
+++ zsh/apply.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+if [ "$(uname)" == "Darwin" ]; then
+	# zsh is default on osx
+	echo "zsh already present"
+else
+	sudo apt-get install -y zsh
+	chsh -s $(which zsh)
+fi
+
 # omz
 if [ ! -d $HOME/.oh-my-zsh ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
