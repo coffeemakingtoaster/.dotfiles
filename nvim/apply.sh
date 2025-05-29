b7969d41004776f93d41b39e720418179054d959
coffeemakingtoaster <coffeemakingtoaster@gmail.com>
2025-05-29 09:59:05 +0200

    feat(nvim): update install process

--- nvim/apply.sh
+++ nvim/apply.sh
@@ -9,14 +9,18 @@ else
 	sudo apt install -y make gcc ripgrep unzip git xclip curl git
 	# Now we install nvim
 	# I use debian and the apt version is too far behind
-	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+	# legacy location
 	sudo rm -rf /opt/nvim-linux64
-	sudo mkdir -p /opt/nvim-linux64
-	sudo chmod a+rX /opt/nvim-linux64
-	sudo tar -C /opt -xzf nvim-linux64.tar.gz
+	# new location
+	sudo rm -rf /opt/nvim-linux-x86_64
+	sudo mkdir -p /opt/nvim-linux-x86_64
+	sudo chmod a+rX /opt/nvim-linux-x86_64
+	sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
-	sudo ln -sf /opt/nvim-linux64/bin/nvim /usr/local/bin/
+	sudo ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/
 
+	rm nvim-linux*
 fi
 
 # Cleanup
