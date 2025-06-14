43be13b076bb10b364175b028d2ef6dc6b1450e1
coffeemakingtoaster <coffeemakingtoaster@gmail.com>
2025-06-14 14:49:58 +0200

    feat(ghostty): add ghostty install on linux if snap is present

--- ghostty/apply.sh
+++ ghostty/apply.sh
@@ -2,14 +2,19 @@
 if [ "$(uname)" == "Darwin" ]; then
 	brew install --cask ghostty 
 else
-	exit 0
+
+	if type snap >/dev/null 2>&1; then
+		sudo snap install ghostty --classic
+	fi
 fi
 
 cp -r ghostty $HOME/.config
 
 # This should (tm) ensure that the application support folder has been created
 # If that is not the case execute the rm command below again after starting ghostty for the first time 
-ghostty +show-config
+if type ghostty >/dev/null 2>&1; then
+	ghostty +show-config
+fi
 
 rm -rf ~/Library/Application Support/com.mitchellh.ghostty/
 
